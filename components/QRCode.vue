faa7e45eaaa966a4efe8b77bdd748154e902d36e
Máté Czékus <mate@picloud.hu>
2025-11-23 20:34:29 +0100

    feat: add QRCode component to be used in MDC/MDX
    Signed-off-by: Máté Czékus <mate@picloud.hu>

---
+++ components/QRCode.vue
@@ -0,0 +1,38 @@
+/** * QRCode Component * * Renders a QR code as an SVG using qrcode.vue. * *
+@example *
+<QRCode value="https://example.com" :size="200" />
+*/
+<template>
+  <qrcode-vue
+    :value="value"
+    :size="size"
+    :margin="margin"
+    :level="level"
+    render-as="svg"
+  />
+</template>
+
+<script setup lang="ts">
+import QrcodeVue from "qrcode.vue"
+
+/** Error correction level for QR codes */
+type ErrorCorrectionLevel = "L" | "M" | "Q" | "H"
+
+interface Props {
+  /** The data to encode in the QR code */
+  value?: string
+  /** The size of the QR code in pixels */
+  size?: number
+  /** The margin around the QR code */
+  margin?: number
+  /** Error correction level */
+  level?: ErrorCorrectionLevel
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  value: "https://adroitgroup.io",
+  size: 300,
+  margin: 3,
+  level: "L",
+})
+</script>
